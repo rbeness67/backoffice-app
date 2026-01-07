c342645f86aa7d5f7ccb407cda4d8b718268ac39
Rayane Benessaidi <rayane.benessaidi@epita.fr>
2026-01-07 15:11:36 +0100

    SaaS reutilisable Clonable avec auth effective

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import authRoutes from "./modules/auth/auth.routes";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use("/auth", authRoutes);
 
