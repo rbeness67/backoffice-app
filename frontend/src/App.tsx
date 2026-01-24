c3eb03fc2013fa0eca98c807f6cc9150faf9ae30
Rayane Benessaidi <rayane.benessaidi@epita.fr>
2026-01-24 07:36:35 +0100

    switch to dark mode

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,9 @@
 import Router from "@/router";
+import { ThemeProvider } from "./components/theme-provider";
 
 export default function App() {
-  return <Router />;
+  
+  return <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router />
+    </ThemeProvider>;
 }
